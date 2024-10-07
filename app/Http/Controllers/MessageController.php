40f565cb7015af8d43006471e01f78d12b8f78be
Agnes <agypeter97@gmail.com>
2024-10-07 19:57:24 +0300

    endpoints to manage msg transfer between users

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
-    //
+    // List messages between users
+    public function index($userId)
+    {
+        $messages = Message::where('sender_id', $userId)
+            ->orWhere('receiver_id', $userId)
+            ->with('sender', 'receiver')
+            ->get();
+        return response()->json($messages);
+    }
+
+    // single message
+    public function show($id)
+    {
+        $message = Message::with('sender', 'receiver')->findOrFail($id);
+        return response()->json($message);
+    }
+
+    // Send message
+    public function store(Request $request)
+    {
+        $request->validate([
+            'sender_id' => 'required|exists:users,id',
+            'receiver_id' => 'required|exists:users,id',
+            'body' => 'required|string',
+        ]);
+
+        $message = Message::create($request->all());
+        return response()->json($message, 201);
+    }
+
+    // Update message
+    public function update(Request $request, $id)
+    {
+        $message = Message::findOrFail($id);
+        $message->update($request->all());
+        return response()->json($message);
+    }
+
+    // Deletemessage
+    public function destroy($id)
+    {
+        Message::destroy($id);
+        return response()->json(null, 204);
+    }
 }
