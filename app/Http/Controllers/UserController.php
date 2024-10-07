0d23f15998bb5806d9ade65b9a32d30ecd783f79
Agnes <agypeter97@gmail.com>
2024-10-07 16:58:45 +0300

    function to create a new user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,11 +7,32 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    //all users
     public function index(){
         $users = User::all();
         return response()->json($users);
+
     }
+
+    // single user
+    public function show($id){
+        $user = User::findOrFail($id);
+        return response()->json($user);
+    }
+
+    // create a new user
+    public function store(Request $request){
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+
+        ]);
+    }
+        
+
+  
+    
 
     
 }
