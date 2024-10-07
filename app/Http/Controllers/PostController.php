2007632484603db966b16f52965bb8ed9ebcc8dd
Agnes <agypeter97@gmail.com>
2024-10-07 19:44:58 +0300

    crud operations functions on post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,46 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function index(){
+        $posts = Post::with('user')->get();
+        return response()->json($posts);
+    }
+
+    // show single post
+    public function show($id){
+        $post = Post::with('user', 'comments', 'likes')->findOrFail($id);
+        return response()->json($post);
+    }
+
+
+    // create new post
+    public function store(Request $request){
+        $request->validate([
+            'title'=>'required|string|max:225',
+            'body' => 'required|string',
+            'user_id' => 'required|exists:users,id',
+        ]);
+
+        $post = Post::create($request->all());
+        return response()->json($post, 201);
+    }
+
+    // post update
+
+    public function update(Request $request, $id){
+        $post = Post::findOrFail($id);
+        $post->update($request->all());
+        return response()->json($post, 200);
+    }
+
+
+    // delete post
+    public function destroy($id){
+        post::destroy($id);
+        return response()->json(null, 204);
+    }
 }
